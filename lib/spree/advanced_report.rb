83d2719ca936b84b8c0ab6ecebbee2a4c59640ce
geermc4 <geermc4@gmail.com>
2015-01-22 15:54:20 -0800

    remove quotes from revenue report

--- lib/spree/advanced_report.rb
+++ lib/spree/advanced_report.rb
@@ -39,9 +39,11 @@ module Spree
       params[:search][:completed_at_not_null] = true
       params[:search][:state_not_eq] = 'canceled'
 
+      params[:search].merge!( { payments_payment_method_name_not_eq: "Quote" } )
+
       search = Order.search(params[:search])
       # self.orders = search.state_does_not_equal('canceled')
-      self.orders = search.result
+      self.orders = search.result.uniq
 
       self.product_in_taxon = true
       if params[:advanced_reporting]
